72aaabe1d2eb0bc3c17a25de06e97aced86563d9
Matt Gauger <matt.gauger@gmail.com>
2014-09-16 22:56:27 -0500

    Update dependencies

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.cemerick/url "0.1.0"]]
+                 [com.cemerick/url "0.1.1"]]
   :profiles {:dev
-             {:dependencies [[midje "1.6.0"]]
+             {:dependencies [[midje "1.6.3"]]
               :plugins [[lein-midje "3.1.1"]]}})
